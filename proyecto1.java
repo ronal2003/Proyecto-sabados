dfa0f941b728f0ee86094c5bc466991441fde21b
ronal2003 <107308574+ronal2003@users.noreply.github.com>
2022-06-18 08:16:48 -0500

    segundo intento

--- proyecto1.java
+++ proyecto1.java
@@ -1,5 +1,6 @@
 public class proyecto1 {
     public static void main(String[] args){
+        System.out.println("MI primer proyecto en github");
         //este es mi nuevo proyecto
     }
-}clec
+}
